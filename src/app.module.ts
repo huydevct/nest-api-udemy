50d3d4aa3d2a0b8e5972cf6ba5db3462a62090d5
huydevct <huyctdev@example.com>
2021-12-15 22:46:24 +0700

    managing app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,33 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,35 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  // it('handles a signup request', () => {
+  //   const email = 'huy1234@huy.com';
+  //   return request(app.getHttpServer())
+  //     .post('/auth/signup')
+  //     .send({ email, password: 'password' })
+  //     .expect(201)
+  //     .then((res) => {
+  //       const { id, email } = res.body;
+  //       expect(id).toBeDefined();
+  //       expect(email).toEqual(email);
+  //     });
+  // });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'huy@huy.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '1234' })
+      .expect(201);
+    
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
+  });
 });
